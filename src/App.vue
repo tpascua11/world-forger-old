f893ea4cb52131ace2ec650a66ac910e029944dc
Ted Pascua <tedlpascua@gmail.com>
2022-06-24 22:30:48 -0700

    route test 3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/test">Test</router-link>
   </nav>
   <router-view />
 </template>
@@ -16,7 +17,7 @@
 }
 
 nav {
-  padding: 30px;
+  padding: 1px;
 }
 
 nav a {
---
+++ src/views/TestView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="Test">
+    <p>Super Test!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestView",
+  components: {},
+};
+</script>
